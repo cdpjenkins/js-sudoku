ccf56910c0d32c069c5751948619d3b03b834b02
Chris Jenkins <cdpjenkins@gmail.com>
2014-09-11 19:46:23 +0100

    Basic board stuff

--- sodoku.js
+++ sodoku.js
@@ -1,4 +1,4 @@
-var b = _.range(1,9)
+var b = _.range(1,10)
 var default_board = [
     [_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b)],
     [_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b),_.clone(b)],
@@ -12,7 +12,24 @@ var default_board = [
 ];
 
 
+var value_at = function(board, x, y) {
+    return board[y][x];
+}
 
+var is_solved_square = function(board, x, y) {
+    if (value_at(board, x, y).length == 1) {
+	return true;
+    } else {
+	return false;
+    }
+}
 
+/*
+var place_number = function(board, x, y, num) {
+    
+
+    return result;
+}
+*/
 
 console.log(default_board);
